3fa55e14c407edd13860dda90b80ce69a5d93ddb
Shimrah07 <shimrahahmedsa27@gmail.com>
2025-01-30 18:01:29 +0530

    updates

--- book-store-app/src/main.jsx
+++ book-store-app/src/main.jsx
@@ -1,17 +1,17 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter, HashRouter} from "react-router-dom"
-
+import {HashRouter} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 import AuthContext from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <HashRouter >
-    <AuthContext>
-    <App />
-    </AuthContext>
-    </HashRouter >
-  // </StrictMode>,
+  <StrictMode>
+    <HashRouter>
+      <AuthContext>
+        <App />
+      </AuthContext>
+    </HashRouter>
+  </StrictMode>,
 )
